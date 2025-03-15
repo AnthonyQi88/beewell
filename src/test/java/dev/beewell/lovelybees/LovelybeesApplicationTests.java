da3a9df739d8e06c6b1cd9331ba6f75a577d2010
Anthony <anthony211407@gmail.com>
2025-03-15 15:04:52 +0900

    First commit

---
+++ src/test/java/dev/beewell/lovelybees/LovelybeesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.beewell.lovelybees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelybeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
