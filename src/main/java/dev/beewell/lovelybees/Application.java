7b75140a77c7386564b4e2a146813bb245764db4
Anthony <anthony211407@gmail.com>
2025-03-15 16:16:56 +0900

    new

--- src/main/java/dev/beewell/lovelybees/Application.java
+++ src/main/java/dev/beewell/lovelybees/Application.java
@@ -1,13 +1,18 @@
 package dev.beewell.lovelybees;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("New Message ");
+
+
 	}
 
 }
